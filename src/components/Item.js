dd246ffd170c6ebe08d6d616adcb9711b50b0843
suin <suinyeze@gmail.com>
2017-10-03 12:12:49 +0900

    Qiita API v2に対応する
    * 投稿日時が表示されない問題を解決した
    * タグのリンク切れを修正した
    * ユーザ名が表示されない問題を解決した
    * いいね数のアイコンをフォルダから親指立てた手に変更
    * ストック数をいいね数に変更した
    
    ただし投稿日はQiitaAPIが返してくるISO 8601形式をそのまま表示する形でその場しのぎの対応になっている。
    
    ref #4

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,16 +2,16 @@ import React from "react";
 import Avatar from "components/Avatar";
 import Tag from "components/Tag";
 import Link from "components/Link";
-import Folder from "components/Folder";
+import ThumbsUp from "components/ThumbsUp";
 
 export default class Item extends React.Component {
   static propTypes = {
     url: React.PropTypes.string.isRequired,
     title: React.PropTypes.string.isRequired,
     tags: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
-    stock_count: React.PropTypes.number.isRequired,
+    likes_count: React.PropTypes.number.isRequired,
     user: React.PropTypes.object.isRequired,
-    created_at_in_words: React.PropTypes.string.isRequired,
+    created_at: React.PropTypes.string.isRequired,
     last: React.PropTypes.bool.isRequired
   };
 
@@ -40,7 +40,7 @@ export default class Item extends React.Component {
         fontSize: 16,
         lineHeight: "20px"
       },
-      stocks: {
+      likes: {
         float: "right",
         fontSize: 12,
         color: "#999",
@@ -58,19 +58,19 @@ export default class Item extends React.Component {
     return (
       <div style={styles.container}>
         <Avatar
-          url={urlBase + "/" + this.props.user.url_name}
+          url={urlBase + "/" + this.props.user.id}
           avatar={this.props.user.profile_image_url} />
         <div style={styles.item}>
-          <div style={styles.stocks}>
-            <Folder />{" "}{this.props.stock_count}
+          <div style={styles.likes}>
+            <ThumbsUp />{" "}{this.props.likes_count}
           </div>
           <div style={styles.itemStatus}>
-            <Link href={urlBase + "/" + this.props.user.url_name} target="_blank">{this.props.user.url_name}</Link>
-            が{this.props.created_at_in_words}前に投稿
+            <Link href={urlBase + "/" + this.props.user.id} target="_blank">{this.props.user.id}</Link>
+            が{this.props.created_at}に投稿
           </div>
           <Link href={this.props.url} style={styles.itemLink} target="_blank">{this.props.title}</Link>
           <div>
-            {this.props.tags.map((tag) => <Tag key={tag.name} name={tag.name} url={urlBase + "/tags/" + tag.url_name} />)}
+            {this.props.tags.map((tag) => <Tag key={tag.name} name={tag.name} url={urlBase + "/tags/" + tag.name} />)}
           </div>
         </div>
       </div>
